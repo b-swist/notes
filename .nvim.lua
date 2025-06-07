945a3f9ea67cfe643aaaafe350a2cb0521fe70d2
b-swist <bswist@protonmail.com>
2025-06-08 01:22:21 +0200

    add spellfile only on files with enabled spell

--- .nvim.lua
+++ .nvim.lua
@@ -1,5 +1,9 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   callback = function()
-    vim.opt_local.spellfile = "./custom.add"
+    vim.schedule(function()
+      if vim.wo.spell then
+        vim.opt_local.spellfile = "./custom.add"
+      end
+    end)
   end,
 })
