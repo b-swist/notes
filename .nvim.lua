dd6d176d8c90bc3404c99c241d9f33082df928d4
b-swist <bswist@protonmail.com>
2025-06-28 20:37:45 +0200

    remove custom spellfile

--- .nvim.lua
+++
@@ -1,9 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-  callback = function()
-    vim.schedule(function()
-      if vim.wo.spell then
-        vim.bo.spellfile = "./custom.add"
-      end
-    end)
-  end,
-})
