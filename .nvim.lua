f351d8e159b892650b5f3b23efc11f54372adb32
b-swist <bswist@protonmail.com>
2025-06-28 16:23:50 +0200

    small fixes

--- .nvim.lua
+++ .nvim.lua
@@ -1,9 +1,9 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   callback = function()
-    vim.defer_fn(function()
+    vim.schedule(function()
       if vim.wo.spell then
         vim.bo.spellfile = "./custom.add"
       end
-    end, 10)
+    end)
   end,
 })
